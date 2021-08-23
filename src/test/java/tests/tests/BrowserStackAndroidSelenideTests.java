47911d508497ed03da7aa36f2a2b2a62aac3d69d
Egor Muratov <egormuratov@yahoo.com>
2021-08-23 22:25:03 +0300

    Created second test

--- src/test/java/tests/tests/BrowserStackAndroidSelenideTests.java
+++ src/test/java/tests/tests/BrowserStackAndroidSelenideTests.java
@@ -1,11 +1,13 @@
 package tests.tests;
 
+import com.codeborne.selenide.Condition;
 import io.appium.java_client.MobileBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
@@ -23,5 +25,22 @@ public class BrowserStackAndroidSelenideTests extends TestBase {
         step("Проверка поиска", () ->
                 $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_container"))
                         .shouldHave(sizeGreaterThan(0)));
+    }
+
+    @Test
+    @DisplayName("Открытие экрана Setting")
+    void openSettingScreen() {
+        step("Тап на кнопку Меню", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/menu_overflow_button")).click();
+        });
+
+        step("Нажать на кнопку Setting", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/explore_overflow_settings")).click();
+        });
+
+        step("Проверится что осуществлен переход к экрану Setting", () -> {
+            $(MobileBy.className("android.widget.TextView")).shouldHave(text("Setting"));
+        });
+
     }
 }
