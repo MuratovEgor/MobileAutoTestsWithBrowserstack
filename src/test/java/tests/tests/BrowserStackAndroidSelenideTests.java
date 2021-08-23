9dcc1687bd5d60350a309082a304b9d4f1c458bf
Egor Muratov <egormuratov@yahoo.com>
2021-08-23 22:26:06 +0300

    Created second test

--- src/test/java/tests/tests/BrowserStackAndroidSelenideTests.java
+++ src/test/java/tests/tests/BrowserStackAndroidSelenideTests.java
@@ -1,6 +1,5 @@
 package tests.tests;
 
-import com.codeborne.selenide.Condition;
 import io.appium.java_client.MobileBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
